dad923a4eb973dbce9a17ccfdf3aa35d917c235c
秋无 <115909059+Jamyein@users.noreply.github.com>
2026-03-21 21:30:01 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.voxly"
         minSdk = 28
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.3.0"
+        versionCode = 29
+        versionName = "1.3.1"
 
         @Suppress("DEPRECATION")
         resourceConfigurations += listOf("en", "zh-rCN")
