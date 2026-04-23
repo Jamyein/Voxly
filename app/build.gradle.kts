122bfd1155dd972a363e1e4277289e0d03bacb47
秋无 <115909059+Jamyein@users.noreply.github.com>
2026-04-23 21:24:20 +0800

    version: bump to 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.voxly"
         minSdk = 30
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.6.7"
+        versionCode = 48
+        versionName = "1.7.0"
 
         @Suppress("DEPRECATION")
         resourceConfigurations += listOf("en", "zh-rCN")
